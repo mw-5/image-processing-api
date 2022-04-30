cba1f5d4dc5ceda43b6464036b3e0b8dbd969938
mw <martinez2@gmx.de>
2022-04-30 18:35:36 +0200

    feat: Set up server

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+// TODO: Add routes
+
+app.listen(port, () => {
+  console.log(`Server listens at http://localhost:${port}`);
+});
