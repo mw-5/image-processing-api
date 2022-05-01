f23cc77f31fd39d2806238514aae7a0d2fc24f38
mw <martinez2@gmx.de>
2022-05-01 15:44:05 +0200

    feat: Add stub for function buildCacheImageName

---
+++ src/utilities/utils.ts
@@ -0,0 +1,15 @@
+/**
+ * @description Build name of cached image based on supplied arguments
+ * @param {string} fileName - The name of the original file with its extension
+ * @param {number} width - The width of the resized image
+ * @param {number} height - The height of the resized image
+ * @returns {string} File name of the resized image
+ */
+export const buildCacheImageName = (
+  fileName: string,
+  width: number,
+  height: number
+): string => {
+  // TODO: implement function
+  return '';
+};
