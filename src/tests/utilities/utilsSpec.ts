bb3ad4ed74880d425616b6575a3bd6eae274c9ee
mw <martinez2@gmx.de>
2022-05-01 21:53:07 +0200

    test: Add multiple . to file name

--- src/tests/utilities/utilsSpec.ts
+++ src/tests/utilities/utilsSpec.ts
@@ -3,10 +3,10 @@ import { buildCacheImageName } from '../../utilities/utils';
 describe('Function that builds names of resized images', () => {
 	it('should build expected name', () => {
 		// Arrange
-		const originalFileName = 'Test.jpg';
+		const originalFileName = 'Te.st.jpg';
 		const width = 100;
 		const height = 200;
-		const expectedFileName = `Test-w${width}-h${height}.jpg`;
+		const expectedFileName = `Te.st-w${width}-h${height}.jpg`;
 
 		// Act
 		const newFileName = buildCacheImageName(
