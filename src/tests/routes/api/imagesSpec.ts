ed13de8fe68284e89c54826e27f1ff01aeabe27b
mw <martinez2@gmx.de>
2022-05-01 15:27:21 +0200

    test: Add test for images endpoint

---
+++ src/tests/routes/api/imagesSpec.ts
@@ -0,0 +1,13 @@
+import supertest from 'supertest';
+import app from '../../../index';
+
+describe('Tests for endpoint GET api/images', () => {
+  const req = supertest(app);
+  const route = 'api/images?a=b';
+
+  it('expects status code 200 on success', () => {
+    req.get(route).end((err, res) => {
+      expect(res.status).toEqual(200);
+    });
+  });
+});
