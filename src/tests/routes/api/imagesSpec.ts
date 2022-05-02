ddaa00b9d14804d594ce5d2663b1b11cb5dade1b
mw <martinez2@gmx.de>
2022-05-02 21:09:43 +0200

    test: Add test whether endpoint returns an image

--- src/tests/routes/api/imagesSpec.ts
+++ src/tests/routes/api/imagesSpec.ts
@@ -1,13 +1,24 @@
 import supertest from 'supertest';
 import app from '../../../index';
 
-describe('Tests for endpoint GET api/images', () => {
+describe('Test for endpoint GET api/images', () => {
 	const req = supertest(app);
-	const route = 'api/images?a=b';
+	const route = 'api/images';
+	const fileName = 'icelandwaterfall';
+	const ext = 'jpg';
+	const width = 100;
+	const height = 200;
+	const qry = `?filename=${fileName}.${ext}&width=${width}&height=${height}`;
 
 	it('expects status code 200 on success', () => {
-		req.get(route).end((err, res) => {
+		req.get(route + qry).end((err, res) => {
 			expect(res.status).toEqual(200);
+		});
+	});
+
+	it('expects response to contain an image', () => {
+		req.get(route + qry).end((err, res) => {
+			expect(res.type).toEqual(`image/${ext}`);
 		});
 	});
 });
