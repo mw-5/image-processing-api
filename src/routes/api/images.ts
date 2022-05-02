333725119829e1c70a333d5aa263ba4d4dc40d29
mw <martinez2@gmx.de>
2022-05-02 21:21:12 +0200

    feat: Implements returning a static image from cache

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,8 +1,15 @@
 import express from 'express';
+import path from 'path';
 
 const images = express.Router();
 
-// Endpoint for serving a scaled image
-images.get('/', (req, res) => res.send('Serve an image'));
+// Endpoint for serving a resized image
+images.get('/', (req, res) =>
+	res.sendFile(
+		path.resolve(
+			'../image-processing-api/assets/thumb/icelandwaterfall-w100-h200.jpg'
+		)
+	)
+);
 
 export default images;
