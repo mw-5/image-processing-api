628f31521c930bcdfbbc70fa4848398fe7aed0fe
mw <martinez2@gmx.de>
2022-05-03 13:50:34 +0200

    test: Refactor tests to use async/await

--- src/tests/routes/api/imagesSpec.ts
+++ src/tests/routes/api/imagesSpec.ts
@@ -1,24 +1,29 @@
 import supertest from 'supertest';
 import app from '../../../index';
 
+const request = supertest(app);
+
 describe('Test for endpoint GET api/images', () => {
-	const req = supertest(app);
-	const route = 'api/images';
-	const fileName = 'icelandwaterfall';
-	const ext = 'jpg';
+	const route = '/api/images';
+	const fileName = 'icelandwaterfall.jpg';
 	const width = 100;
 	const height = 200;
-	const qry = `?filename=${fileName}.${ext}&width=${width}&height=${height}`;
+	const qry = `?filename=${fileName}&width=${width}&height=${height}`;
 
-	it('expects status code 200 on success', () => {
-		req.get(route + qry).end((err, res) => {
-			expect(res.status).toEqual(200);
-		});
+	it('expects status code 200 on success', async () => {
+		const response = await request.get(route + qry);
+		expect(response.statusCode).toEqual(200);
 	});
 
-	it('expects response to contain an image', () => {
-		req.get(route + qry).end((err, res) => {
-			expect(res.type).toEqual(`image/${ext}`);
-		});
+	it('expects response to contain an image', async () => {
+		const response = await request.get(route + qry);
+		expect(response.type).toEqual('image/jpeg');
+	});
+
+	it('expects status code 404 if image is not found', async () => {
+		const response = await request.get(
+			route + '?filename=noimage.jpg&width=1&height=1'
+		);
+		expect(response.statusCode).toEqual(404);
 	});
 });
