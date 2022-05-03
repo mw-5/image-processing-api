b3b84fd50fbf3f2e88f8e9a25810078b0f16cd2e
mw <martinez2@gmx.de>
2022-05-03 22:09:15 +0200

    refactor: Implement use of qry args for endpoint
    Implements use of arguments provided by query string to
    determine which thumb should be served.

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,15 +1,88 @@
 import express from 'express';
 import path from 'path';
+import { buildCacheImageName, isNumber } from '../../utilities/utils';
 
 const images = express.Router();
+const dirThumbs = '../image-processing-api/assets/thumb';
+const PARAM_FILENAME = 'filename';
+const PARAM_WIDTH = 'width';
+const PARAM_HEIGHT = 'height';
+
+/**
+ * @description Validates the input for the endpoint GET /api/images
+ * @param {express.Request} req - The incoming request
+ * @returns - Returns a tuple consisting of whether the input is valid,
+ * the status code to be returned if invalid and a description of the errors
+ */
+const validateInput = (req: express.Request): [boolean, number, string] => {
+	let isValid = true;
+	let statusCode = 0;
+	let msg = '';
+	const missingArgs: string[] = [];
+	const MISSING = ' is missing';
+	const NAN = ' is not a number';
+
+	// Check filename
+	if (req.query.filename === undefined) {
+		missingArgs.push(PARAM_FILENAME + MISSING);
+	}
+
+	// Check width
+	if (req.query.width === undefined) {
+		missingArgs.push(PARAM_WIDTH + MISSING);
+	} else if (!isNumber(<string>req.query.width)) {
+		missingArgs.push(PARAM_WIDTH + NAN);
+	}
+
+	// Check height
+	if (req.query.height === undefined) {
+		missingArgs.push(PARAM_HEIGHT + MISSING);
+	} else if (!isNumber(<string>req.query.height)) {
+		missingArgs.push(PARAM_HEIGHT + NAN);
+	}
+
+	// If arguments are missing set info for user
+	if (missingArgs.length > 0) {
+		// Build message for user
+		msg = 'Failed to serve image.\n' + 'These parameters caused errors:\n';
+		missingArgs.forEach((param) => (msg += `- ${param}\n`));
+
+		// Set status code to 400 bad request
+		statusCode = 400;
+
+		// Flag input as invalid
+		isValid = false;
+	}
+
+	return [isValid, statusCode, msg];
+};
 
 // Endpoint for serving a resized image
-images.get('/', (req, res) =>
-	res.sendFile(
-		path.resolve(
-			'../image-processing-api/assets/thumb/icelandwaterfall-w100-h200.jpg'
-		)
-	)
-);
+images.get('/', (req, res) => {
+	try {
+		// Check input
+		const [isValid, statusCode, errMsg] = validateInput(req);
+
+		// Inform user if input is invalid
+		if (!isValid) {
+			res.statusCode = statusCode;
+			res.send(errMsg);
+			return;
+		}
+
+		// Serve cached file
+		const thumbName = buildCacheImageName(
+			<string>req.query.filename,
+			<number>(<unknown>req.query.width),
+			<number>(<unknown>req.query.height)
+		);
+		res.sendFile(path.resolve(dirThumbs, thumbName));
+	} catch (err) {
+		console.log(err);
+		// Hide server internals from user
+		res.statusCode = 500;
+		res.send('Sorry, something went wrong.');
+	}
+});
 
 export default images;
