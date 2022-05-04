762d5cd4f211af792fccef27af5e762ecf32f363
mw <martinez2@gmx.de>
2022-05-04 10:48:11 +0200

    test: Tests use query params provided by endpoint

--- src/tests/routes/api/imagesSpec.ts
+++ src/tests/routes/api/imagesSpec.ts
@@ -1,5 +1,10 @@
 import supertest from 'supertest';
 import app from '../../../index';
+import {
+	PARAM_FILENAME,
+	PARAM_WIDTH,
+	PARAM_HEIGHT,
+} from '../../../routes/api/images';
 
 const request = supertest(app);
 
@@ -8,7 +13,7 @@ describe('Test for endpoint GET api/images', () => {
 	const fileName = 'icelandwaterfall.jpg';
 	const width = 100;
 	const height = 200;
-	const qry = `?filename=${fileName}&width=${width}&height=${height}`;
+	const qry = `?${PARAM_FILENAME}=${fileName}&${PARAM_WIDTH}=${width}&${PARAM_HEIGHT}=${height}`;
 
 	it('expects status code 200 on success', async () => {
 		const response = await request.get(route + qry);
@@ -22,19 +27,23 @@ describe('Test for endpoint GET api/images', () => {
 
 	it('expects status code 404 if image is not found', async () => {
 		const response = await request.get(
-			route + '?filename=noimage.jpg&width=1&height=1'
+			route +
+				`?${PARAM_FILENAME}=noimage.jpg&${PARAM_WIDTH}=1&${PARAM_HEIGHT}=1`
 		);
 		expect(response.statusCode).toEqual(404);
 	});
 
 	it('expects status code 400 for missing parameters', async () => {
-		const response = await request.get(route + `?filename=${fileName}`);
+		const response = await request.get(
+			route + `?${PARAM_FILENAME}=${fileName}`
+		);
 		expect(response.statusCode).toEqual(400);
 	});
 
 	it('expects status code 400 for width or height not being a number', async () => {
 		const response = await request.get(
-			route + `?filename=${fileName}&width=a&height=200px`
+			route +
+				`?${PARAM_FILENAME}=${fileName}&${PARAM_WIDTH}=a&${PARAM_HEIGHT}=200px`
 		);
 		expect(response.statusCode).toEqual(400);
 	});
